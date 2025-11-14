c4996997ee6cb412aecafb767c073b8f903e74ce
Roland417 <rolandchurchboy417@gmail.com>
2025-11-14 10:21:32 +0100

    Add files via upload

---
+++ NumberConversion.py
@@ -0,0 +1,14 @@
+number = 42339 
+
+# Convert the number to a string first 
+
+number_str = str(number)
+
+# Join each character (digit) with a space
+
+spaced_digits = " ".join(number_str)
+
+print(spaced_digits)
+
+#Output: 4 2 3 3 9
+
